a05a2738e201f5a7e5cd454da45db1a26a2e8593
akashroy18 <akash02052005@gmail.com>
2026-01-23 21:04:21 +0530

    inorder traversal is done and its verified that it is BST cause it is giving sorted sequence

--- BST/basic.java
+++ BST/basic.java
@@ -20,7 +20,21 @@ public class basic {
         }
         return root;
     }
+    public static void inorder(Node root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
     public static void main(String[] args) {
         int val[] = {5,1,3,4,2,7};
+        Node root = null;
+        for(int i=0;i<val.length;i++){
+            root=insert(root, val[i]);
+        }
+        System.out.println("inorder traversal ");
+        inorder(root);
     }
 }
