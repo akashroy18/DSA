9c40661e339bfb397eb55faab6d841ae7fc88aba
akashroy18 <akash02052005@gmail.com>
2026-01-29 18:13:19 +0530

    Majorty element with hashmap implementation with time complexity of o(n)

---
+++ Hashmap/Majorityel.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Majorityel {
+    public static void Majority(int[] arr){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i],map.get(arr[i])+1);
+            }
+            else{
+                map.put(arr[i],1);
+            }
+        }
+        for(int key:map.keySet()){
+            if(map.get(key)>n/3){
+                System.out.print(key+" ");
+            }
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[]={1,3,2,5,1,3,1,5,1};  
+        Majority(arr);  
+    }
+    
+}
