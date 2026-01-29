93f24c8d9d43cdd7b56c0262e9b6eaa5c0e4674a
akashroy18 <akash02052005@gmail.com>
2026-01-29 18:30:16 +0530

    Intersection of 2 set also implemented here

--- Hashmap/Union.java
+++ Hashmap/Union.java
@@ -10,10 +10,25 @@ public class Union {
         }
         return set.size();
     }
+    public static int intersection(int[] arr1,int[] arr2){
+        int count=0;
+        HashSet<Integer> h1 = new HashSet<>();
+        for(int i=0;i<arr1.length;i++){
+            h1.add(arr1[i]);
+        }
+        for(int j=0;j<arr2.length;j++){
+            if(h1.contains(arr2[j])){
+                count++;
+                h1.remove(arr2[j]);
+            }
+        }
+        return count;
+    }
     public static void main(String[] args) {
-        int[] arr1={7,3,9};
+        int[] arr1={7,3};
         int[] arr2={6,3,9,2,9,7};
         System.out.println(uni(arr1, arr2));
+        System.out.println(intersection(arr1, arr2));
     }
     
 }
