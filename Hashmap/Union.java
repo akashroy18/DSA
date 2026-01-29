66b2bc57886e150995eed6acbb53c8d4f13cd01e
akashroy18 <akash02052005@gmail.com>
2026-01-29 18:22:28 +0530

    union of 2 set implemented using Hashset with time complexity of o(n)

---
+++ Hashmap/Union.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Union {
+    public static int uni(int[] arr1,int[] arr2){
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<arr1.length;i++){
+            set.add(arr1[i]);
+        }
+        for(int j=0;j<arr2.length;j++){
+            set.add(arr2[j]);
+        }
+        return set.size();
+    }
+    public static void main(String[] args) {
+        int[] arr1={7,3,9};
+        int[] arr2={6,3,9,2,9,7};
+        System.out.println(uni(arr1, arr2));
+    }
+    
+}
